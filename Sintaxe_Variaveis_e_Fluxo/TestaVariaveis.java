97d61b257990fbf21551e05de9dbdba44c8b039c
Jonatham Mayer <mayer.techdev@gmail.com>
2023-02-12 01:00:25 -0300

    Variáveis e concatenação

---
+++ Sintaxe_Variaveis_e_Fluxo/TestaVariaveis.java
@@ -0,0 +1,13 @@
+package Sintaxe_Variaveis_e_Fluxo;
+
+public class TestaVariaveis {
+
+    public static void main(String[] args) {
+        System.out.println("Ola novo teste");
+
+        int idade;
+        idade = 26;
+        System.out.println("A minha idade é: " + idade);
+
+    }
+}
